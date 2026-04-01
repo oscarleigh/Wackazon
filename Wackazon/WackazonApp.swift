217ff558fede8157996c6c65e4d8db6d740b9833
oscarleigh <oscarleigh@icloud.com>
2026-04-01 23:35:14 +0100

    Initial Commit

---
+++ Wackazon/WackazonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WackazonApp.swift
+//  Wackazon
+//
+//  Created by Oscar Leigh on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct WackazonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
