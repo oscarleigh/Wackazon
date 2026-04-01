2d37002727ad67c9bf576b1105c5c1ba22b60ac4
oscarleigh <oscarleigh@icloud.com>
2026-04-02 00:56:02 +0100

    file restructuring

---
+++ Wackazon/Views/RootView.swift
@@ -0,0 +1,17 @@
+//
+//  RootView.swift
+//  Wackazon
+//
+//  Created by Oscar Leigh on 02/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct YourAppNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
